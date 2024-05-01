967384198dad85b9a19fdec265c57b7ef3c5cf9f
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-01 00:34:08 +0000

    actors ready

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,8 +4,8 @@ class ActorsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("the_id")
-    matching_records = Actor.where({ :id => the_id })
+    @the_id = params.fetch("the_id")
+    matching_records = Actor.where({ :id => @the_id })
 
     @the_actor = matching_records.at(0)
     render({ :template => "actor_templates/details" })
